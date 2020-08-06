2e1fa7adff9fc1a36afe314b71091b5b91e4c381
Johnnyj2608 <54607786+Johnnyj2608@users.noreply.github.com>
2020-08-06 01:37:30 -0400

    Add files via upload

--- MenuAccess.java
+++ MenuAccess.java
@@ -1,3 +1,5 @@
+package NewGarden;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -7,21 +9,20 @@ import java.util.ArrayList;
 public class MenuAccess {
 
     private static Connection connection;
-    private static PreparedStatement getFoodItem;
-    private static PreparedStatement updateFoodFreq;
+    private static PreparedStatement Pstatement;
     private static ResultSet resultSet;
     
-    public static ArrayList<MenuProperties> getItem(String item) {
+    public static ArrayList<MenuProperties> getItemSizes(String item) {
         connection = DBConnection.getConnection();
-        ArrayList<MenuProperties> food = new ArrayList<MenuProperties>();
+        ArrayList<MenuProperties> food = new ArrayList<>();
         try {
-            getFoodItem = connection.prepareStatement("SELECT * FROM menu WHERE item = (?)");
-            getFoodItem.setString(1, item);
-            resultSet = getFoodItem.executeQuery();
+            Pstatement = connection.prepareStatement("SELECT * FROM menu WHERE item = (?)");
+            Pstatement.setString(1, item);
+            resultSet = Pstatement.executeQuery();
             
             while(resultSet.next()) {
-                MenuProperties entry = new MenuProperties(resultSet.getString(1), resultSet.getDouble(2), 
-                    resultSet.getDouble(3), resultSet.getDouble(4), resultSet.getDouble(5), resultSet.getInt(6));
+                MenuProperties entry = new MenuProperties(resultSet.getString(1), resultSet.getDouble(2), resultSet.getDouble(3), 
+                        resultSet.getDouble(4), resultSet.getDouble(5), resultSet.getInt(6), resultSet.getString(7));
                 food.add(entry);
             }
         }
@@ -35,14 +36,49 @@ public class MenuAccess {
         connection = DBConnection.getConnection();
         int count = 0;
         try {
-            updateFoodFreq = connection.prepareStatement("UPDATE menu SET amount = (?) where item = (?)");
-            updateFoodFreq.setInt(1, amount);
-            updateFoodFreq.setString(2, item);
-            count = updateFoodFreq.executeUpdate();
+            Pstatement = connection.prepareStatement("UPDATE menu SET amt = (?) where item = (?)");
+            Pstatement.setInt(1, amount);
+            Pstatement.setString(2, item);
+            count = Pstatement.executeUpdate();
         }
         catch(SQLException sqlException) {
             sqlException.printStackTrace();
         }
         return count;
+    }
+    
+    public static ArrayList<String> getAllFromCateg(String categ) {
+        connection = DBConnection.getConnection();
+        ArrayList<String> food = new ArrayList<>();
+        try {
+            Pstatement = connection.prepareStatement("SELECT item FROM menu WHERE categ = (?)");
+            Pstatement.setString(1, categ);
+            resultSet = Pstatement.executeQuery();
+            
+            while(resultSet.next()) {
+                food.add(resultSet.getString(1));
+            }
+        }
+        catch(SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+        return food;
+    }
+    
+    public static ArrayList<String> getAllCateg() {
+        connection = DBConnection.getConnection();
+        ArrayList<String> food = new ArrayList<>();
+        try {
+            Pstatement = connection.prepareStatement("SELECT DISTINCT categ FROM menu");
+            resultSet = Pstatement.executeQuery();
+            
+            while(resultSet.next()) {
+                food.add(resultSet.getString(1));
+            }
+        }
+        catch(SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+        return food;
     }
 }
--- MenuProperties.java
+++ MenuProperties.java
@@ -1,12 +1,15 @@
+package NewGarden;
+
 
 public class MenuProperties {
     
-    private String food;
-    private double sizeOne;
-    private double sizeTwo;
-    private double sizeThree;
-    private double sizeFour;
-    private int amount;
+    private final String food;
+    private final double sizeOne;
+    private final double sizeTwo;
+    private final double sizeThree;
+    private final double sizeFour;
+    private final int amount;
+    private final String categ;
     
     public String getName(){
         return food;
@@ -32,17 +35,23 @@ public class MenuProperties {
         return amount;
     }
     
+    public String getCateg() {
+        return categ;
+    }
+    
+    @Override
     public String toString(){
-        return String.format("%s\t %d\t %d%\t %d\t %d\t %d\n",
-                getName(), getSizeOne(), getSizeTwo(), getSizeThree(), getSizeFour(), getAmount());
+        return String.format("%s\t %d\t %d%\t %d\t %d\t %d\t %s\n",
+                getName(), getSizeOne(), getSizeTwo(), getSizeThree(), getSizeFour(), getAmount(), getCateg());
     }
 
-    public MenuProperties(String food, double sizeOne, double sizeTwo, double sizeThree, double sizeFour, int amount) {
+    public MenuProperties(String food, double sizeOne, double sizeTwo, double sizeThree, double sizeFour, int amount, String categ) {
         this.food = food;
         this.sizeOne = sizeOne;
         this.sizeTwo = sizeTwo;
         this.sizeThree = sizeThree;
         this.sizeFour = sizeFour;
         this.amount = amount;
+        this.categ = categ;
     }
 }
